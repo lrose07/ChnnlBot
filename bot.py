90659781dd448693c5e37a30107d321f38a8586f
Lauren Rose <larose@radford.edu>
2019-09-24 12:55:51 -0400

    add guild name output

--- bot.py
+++ bot.py
@@ -5,12 +5,17 @@ import dotenv
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("connected")
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run(token)
+    print(f'{guild.name}(id: {guild.id})')
+
+client.run(TOKEN)
