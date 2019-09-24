ea56cb6c24f10d4f1844316390d71f169f75e2a7
Lauren Rose <larose@radford.edu>
2019-09-24 11:59:43 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+
+import discord
+import dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("connected")
+
+client.run(token)
